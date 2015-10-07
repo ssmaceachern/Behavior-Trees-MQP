3c00bb74afc4d70c76cfa0fd76809737510e5a8f
Chris Gillis <froman44@gmail.com>
2015-10-07 15:07:05 -0400

    The freeze feature and the updated peasants

---
+++ KillTheKing/Assets/Scripts/KilledByPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Die when the player enters your trigger 
+public class KilledByPlayer : MonoBehaviour 
+{
+	void Start()
+	{
+
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Destroy (this.gameObject);
+		}
+	}
+}
