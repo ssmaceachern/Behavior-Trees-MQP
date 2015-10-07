dcc289f4949e2077abc66a300ed22bc95eba123f
Sean MacEachern <ssmaceachern@wpi.edu>
2015-10-07 13:14:13 -0400

    Added PieMenu feature

---
+++ Assets/Scripts/PieMenu/TestCommand.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCommand : MonoBehaviour {
+
+    void OnSelect(string command)
+    {
+        Debug.Log("A Menu Command Received: " + command);
+    }
+}
+
