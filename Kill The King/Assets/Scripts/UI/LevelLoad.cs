ad2c51c893151295489584511a0ab0cb0a79a159
unknown <ssmaceachern@github.com>
2015-11-04 00:13:14 -0500

    Added Main Menu and Level Options

---
+++ Kill The King/Assets/Scripts/UI/LevelLoad.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoad : MonoBehaviour {
+
+    public void LoadLevel(int level)
+    {
+        Application.LoadLevel(level);
+    }
+}
