738831130d4b47deb243dfd3e746a3310b6b782f
unknown <ssmaceachern@FL222-07.admin.wpi.edu>
2015-11-11 15:51:30 -0500

    Modified Quest UI -ssmaceachern

--- ATerm_Final/Assets/Scripts/FadeQuestUI.cs
+++ ATerm_Final/Assets/Scripts/FadeQuestUI.cs
@@ -3,22 +3,35 @@ using System.Collections;
 
 using UnityEngine.UI;
 
-public class FadeQuestUI : MonoBehaviour {
+public class FadeQuestUI : MonoBehaviour
+{
 
     public float Duration = 5f;
+    public float FadeOutDuration = 2f;
     public CanvasGroup QuestUI;
 
     private float TimeLeft;
+    private float Fade;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         TimeLeft = Duration;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        TimeLeft -= Time.deltaTime;
+        Fade = FadeOutDuration;
+    }
 
-        QuestUI.alpha = TimeLeft/10f;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimeLeft < 0)
+        {
+            FadeOutDuration -= Time.deltaTime;
+            QuestUI.alpha = FadeOutDuration;
+        }
+        else
+        {
+            TimeLeft -= Time.deltaTime;
+        }
+
+    }
 }
