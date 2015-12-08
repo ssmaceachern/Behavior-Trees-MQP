b315cd669cc4c07071a34047fdbb6d94a8006cde
unknown <ssmaceachern@github.com>
2015-12-08 11:52:48 -0500

    Added base components of level system. Not yet fully functioning @ssmaceachern

---
+++ KillTheKing/Assets/Scripts/LevelSystem/LevelCoordinator.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelCoordinator : MonoBehaviour {
+
+    static LevelCoordinator _instance;
+
+    //Dictionary<Scene Name, Scene Level Info>
+    static Dictionary<string, LevelInfo> LevelRegistry;
+
+    //Holds index of scene to be loaded
+    private int LevelToBeLoaded
+    {
+        set
+        {
+            LevelToBeLoaded = value;
+        }
+    }
+
+    static public bool isActive
+    {
+        get
+        {
+            return _instance != null;
+        }
+    }
+
+    static public LevelCoordinator instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(LevelCoordinator)) as LevelCoordinator;
+
+                if(_instance == null)
+                {
+                    GameObject lc = new GameObject("Level Coordinator");
+                    DontDestroyOnLoad(lc);
+                    _instance = lc.AddComponent<LevelCoordinator>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        LevelRegistry = new Dictionary<string, LevelInfo>();
+    }
+
+    void PopulateLevelRegistry()
+    {
+        
+        string LevelRegistryPath = Application.dataPath + "/Scenes/MainLevels/";
+        Debug.Log(LevelRegistryPath);
+
+        string[] MainLevelNames = Directory.GetFiles(LevelRegistryPath, "*.unity");
+        Debug.Log(MainLevelNames);
+
+        foreach(string MainLevelName in MainLevelNames)
+        {
+            Debug.Log(MainLevelName);
+            LevelRegistry.Add(MainLevelName, new LevelInfo(MainLevelName));
+        }
+
+        Debug.Log(LevelRegistry.Count);
+    }
+
+    void LoadLevel(int level)
+    {
+        PlayerPrefs.SetInt("LastLevel", level);
+        Application.LoadLevel(level);
+    }
+
+    void LoadLevel(string level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    // Use this for initialization
+    void Start () {
+        PopulateLevelRegistry();
+        //Debug.Log(instance == null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ KillTheKing/Assets/Scripts/LevelSystem/LevelInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Collections;
+
+public class LevelInfo {
+
+    //Filepath
+    string path;
+
+    //Info to be loaded
+    int index;
+
+    string  LevelTitle,
+            LevelDescription,
+            ObjectiveTitle,
+            ObjectiveDescription;
+
+    //Constructor
+    public LevelInfo(string SceneName)
+    {
+        if (File.Exists(path))
+        {
+            // Read the names of all levels from the levels file.
+            using (FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    // Possibly use ReadToEnd and string.Split(fileContent, Environment.NewLine).
+                    while (!reader.EndOfStream)
+                    {
+                        levelNames.Add(reader.ReadLine());
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Levels file does not exist, no level names available at run-time.");
+        }
+    }
+
+}
