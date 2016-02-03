e46e3bf18816d109a33a1595ac77de9e76eb2249
Chris Gillis <froman44@gmail.com>
2016-02-03 15:40:43 -0500

    Added in thought bubble functionality.

--- KillTheKing/Assets/AI/Actions/SpawnAlert.cs
+++ KillTheKing/Assets/AI/Actions/SpawnAlert.cs
@@ -8,12 +8,8 @@ using RAIN.Core;
 public class SpawnAlert : RAINAction
 {
     public override ActionResult Execute(RAIN.Core.AI ai)
-    {
-		GameObject particle = (GameObject)GameObject.Instantiate (Resources.Load ("Alert"));
-		particle.transform.position = new Vector3 (ai.Body.transform.position.x, ai.Body.transform.position.y+5.0f, ai.Body.transform.position.z);
-		particle.GetComponent<ParticleFade> ().followTarget = ai.Body;
+	{
 
         return ActionResult.SUCCESS;
     }
-
 }
