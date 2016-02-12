ea766b37dd589f90087e231d4ade07e903c4f844
unknown <ssmaceachern@github.com>
2016-02-12 18:00:46 -0500

    Got circular bars to work. Just need to tweak it to allow multiple to be displayed at once -ssmaceachern

---
+++ KillTheKing/Assets/Scripts/UI/CircularHealthBar.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+[RequireComponent(typeof(Scrollbar))]
+public class CircularHealthBar : MonoBehaviour
+{
+    [SerializeField]
+    Image CircleImage;
+    [SerializeField]
+    Color start;
+    [SerializeField]
+    Color end;
+
+    [SerializeField]
+    Color current;
+
+    [SerializeField]
+    float BarBegin;
+
+    [SerializeField]
+    float BarEnd;
+
+    Scrollbar scrollbar { get { return GetComponent<Scrollbar>(); } }
+
+    void Start()
+    {
+        CircleImage.type = Image.Type.Filled;
+        CircleImage.fillMethod = Image.FillMethod.Radial360;
+        //CircleImage.fillOrigin = BarBegin;
+    }
+
+    void Update()
+    {
+        CircleImage.fillAmount = Mathf.Clamp(scrollbar.value, BarBegin, BarEnd);
+        CircleImage.color = Color.Lerp(start, end, scrollbar.value);
+        current = Color.Lerp(start, end, scrollbar.value);
+    }
+
+
+}
