ca2e560043fffde9ebedfdd396465e89ab8d1b24
christophersgillis <christophersgillis@gmail.com>
2016-02-13 17:55:27 -0500

    Added in the beginnings of tool tips.

---
+++ KillTheKing/Assets/Scripts/Test/InitToolTips.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Initialize the tool tips.
+public class InitToolTips : MonoBehaviour
+{
+    private PieMenu pm;                 // The pie menu of the object we're on. Used to grab icons for actions.
+    private SpriteRenderer[] sprites;   // The sprites of the icons representing actions in the pie menu.
+
+	// Use this for initialization
+	void Start ()
+    {
+        pm = transform.parent.gameObject.GetComponent<PieMenu>();
+
+        sprites = GetComponentsInChildren<SpriteRenderer>();
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            if (pm.icons[i] != null)
+            {
+                Texture2D tex2d = (Texture2D)pm.icons[i];
+                Debug.Log(pm.icons[i].name);
+                sprites[i].sprite = Sprite.Create(tex2d, new Rect(0, 0, tex2d.width, tex2d.height), new Vector2());
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ KillTheKing/Assets/Scripts/Test/ToggleToolTips.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleToolTips : MonoBehaviour
+{
+    private GameObject toolTips;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Ray cast down and see if we hit an entity
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 200.0f) != false)
+        {
+            if (hit.collider.gameObject == transform.parent.gameObject)
+            {
+
+            }
+        }
+    }
+}
