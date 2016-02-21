2f2f052da4d36db10d07344c04acb8328bd6d265
Chris Gillis <froman44@gmail.com>
2016-02-21 18:50:06 -0500

    Added a restart button to the pause menu. Began changing tool tips.

--- KillTheKing/Assets/Scripts/LevelLoad.cs
+++ KillTheKing/Assets/Scripts/LevelLoad.cs
@@ -3,24 +3,34 @@ using System.Collections;
 
 public class LevelLoad : MonoBehaviour 
 {
+	// Load a given level by index
     public void LoadLevel(int level)
     {
 		PlayerPrefs.SetInt ("LastLevel", level);
         Application.LoadLevel(level);
     }
 
+	// Load a given level by name
 	public void LoadLevel(string level)
 	{
 		Application.LoadLevel(level);
 	}
 
+	// Load a given level by name without needing to define a LevelLoad object
     public static void StaticLoadLevel(string level)
     {
         Application.LoadLevel(level);
     }
 
+	// Load the level that was last loaded before the level we are in currently
     public void LoadLastLevel()
 	{
 		Application.LoadLevel (PlayerPrefs.GetInt ("LastLevel"));
+	}
+
+	// Load the level we are currently on to reset the game.
+	public void ReloadLevel()
+	{
+		Application.LoadLevel (Application.loadedLevel);
 	}
 }
