85c43c18e90217360d6ce7fff4f579bbbc145bd6
unknown <ssmaceachern@github.com>
2016-03-19 11:57:09 -0400

    Fixed King HUD GUI glitch and added in smooth movement to King on button press. Also gave general health bars to all knights

--- KillTheKing/Assets/Scripts/UI/DisplayKnightAttributes.cs
+++ KillTheKing/Assets/Scripts/UI/DisplayKnightAttributes.cs
@@ -6,20 +6,10 @@ using System;
 
 public class DisplayKnightAttributes : MonoBehaviour {
 
-	private enum DisplayMode{
-		None 		= 0,
-        Health      = 1,
-        Hunger 		= 2,
-		Greed 		= 3
-	};
-
     //int DisplayModeSize = Enum.GetNames(typeof(DisplayMode)).Length;
 	
 	//The AIRig of the parent knight object
 	AIRig tRig;
-
-	//Enum for switching between modes
-	DisplayMode currentMode;
 
 	//Holds the name of the current mode
 	string currentModeName;
@@ -33,85 +23,15 @@ public class DisplayKnightAttributes : MonoBehaviour {
 	public Slider SliderReference;
 
 	public Scrollbar HealthScrollbarReference;
-	public Scrollbar LoyaltyScrollbarReference;
-	public Scrollbar HungerScrollbarReference;
-	public Scrollbar FearScrollbarReference;
-
-	public Image FillReference;
 
 	// Use this for initialization
 	void Start () {
-		currentMode = DisplayMode.Health;
-		tRig = transform.parent.parent.GetComponentInChildren<AIRig>();
-        SwitchMode();
-    }
-
-	void SwitchMode(){
-
-		switch(currentMode){
-		case DisplayMode.None:
-			
-			currentModeName = "None";
-			FillReference.color = Color.black;
-			break;
-		case DisplayMode.Hunger:
-			
-			currentModeName = "Hunger";
-			FillReference.color = Color.green;
-			break;
-		case DisplayMode.Health:
-			
-			currentModeName = "Health";
-			FillReference.color = Color.red;
-			break;
-		case DisplayMode.Greed:
-			
-			currentModeName = "Loyalty";
-			FillReference.color = Color.blue;
-			break;
-		default:
-			break;
-		}
-
-		//currentModeName = Enum.GetName(typeof(DisplayMode), Convert.ChangeType(currentMode, currentMode.GetTypeCode()));
-		SliderReference.value = tRig.AI.WorkingMemory.GetItem<System.Int32>(currentModeName);
-	}
+		tRig = transform.parent.parent.GetComponentInChildren<AIRig>(); 
+    }	
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Q)){
-
-            if((int)currentMode > 1)
-            {
-                currentMode--;
-            }
-            else
-            {
-                currentMode = DisplayMode.Greed;
-            }
-
-			SwitchMode();
-		}
-
-		if(Input.GetKeyDown(KeyCode.E)){
-
-            if ((int)currentMode < 3)
-            {
-                currentMode++;
-            }
-            else
-            {
-                currentMode = DisplayMode.Health;
-            }
-
-            SwitchMode();
-		}
-
-		SliderReference.value = tRig.AI.WorkingMemory.GetItem<System.Int32>(currentModeName);
-		HealthScrollbarReference.value = tRig.AI.WorkingMemory.GetItem<System.Int32>("Health") / (100f * 5f);
-		LoyaltyScrollbarReference.value = tRig.AI.WorkingMemory.GetItem<System.Int32>("Loyalty") / (100f * 5f);
-		HungerScrollbarReference.value = tRig.AI.WorkingMemory.GetItem<System.Int32>("Hunger") / (100f * 5f);
-		FearScrollbarReference.value = tRig.AI.WorkingMemory.GetItem<System.Int32>("Fear") / (100f * 5f);
+		HealthScrollbarReference.value = tRig.AI.WorkingMemory.GetItem<System.Int32>("Health") / (100f);
 	}
 
 }
