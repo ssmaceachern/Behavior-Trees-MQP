2f6376d33c3e7bd12e9231e9abc326217782bdfb
unknown <csgillis@FL222-13.admin.wpi.edu>
2016-04-03 19:14:19 -0400

    Added a tool tip section to the HUD

--- KillTheKing/Assets/Scripts/Test/ToolTips/ToolTipManager.cs
+++ KillTheKing/Assets/Scripts/Test/ToolTips/ToolTipManager.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ToolTipManager : MonoBehaviour
 {
+    // The HUD element that we will use to display the tool tip
+    public GameObject toolTipHUD;
+
     // The game object whose tool tips we are currently displaying
     private GameObject toolTipEntity;
     private MessageDispatcher dispatch;
@@ -33,6 +37,7 @@ public class ToolTipManager : MonoBehaviour
                 // If so, keep a reference to the tool tip so we don't activate two at once.
                 toolTipEntity = hit.collider.gameObject.GetComponentInChildren<ToolTipMessageReceiver>().gameObject;
 
+                FillInHUD();
                 // Send a message to activate the tool tip
                 if (dispatch != null)
                 {
@@ -53,6 +58,7 @@ public class ToolTipManager : MonoBehaviour
                 // we hit.
                 if (dispatch != null && toolTipEntity != null)
                 {
+                    UnfillHUD();
                     dispatch.SendMsg(0.0f,
                                      this.gameObject,
                                      toolTipEntity,
@@ -63,6 +69,26 @@ public class ToolTipManager : MonoBehaviour
                     toolTipEntity = null;
                 }
             }
+        }
+    }
+
+    void FillInHUD()
+    {
+        // Display the tool tip to the HUD
+        if (toolTipHUD != null)
+        {
+            toolTipHUD.transform.FindChild("UnitName").GetComponent<Text>().text = toolTipEntity.transform.FindChild("UnitName").GetComponent<TextMesh>().text;
+            toolTipHUD.transform.FindChild("ActionDescription").GetComponent<Text>().text = toolTipEntity.transform.FindChild("ActionDescription").GetComponent<TextMesh>().text;
+        }
+    }
+
+    void UnfillHUD()
+    {
+        if (toolTipHUD != null)
+        {
+            toolTipHUD.transform.FindChild("UnitName").GetComponent<Text>().text = "";
+            toolTipHUD.transform.FindChild("ActionDescription").GetComponent<Text>().text = "";
+            ;
         }
     }
 }
